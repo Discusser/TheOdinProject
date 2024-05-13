7f9a2014ad9b89d6d182643fa7c349d6d8e30bc3
Discusser <47938380+Discusser@users.noreply.github.com>
2024-05-13 08:29:56 +0200

    Complete tasks from javascript.info on variables

---
+++ foundations-course/variables-and-operators/script.js
@@ -0,0 +1,14 @@
+// javascript.info Task 1
+// let admin;
+// let name;
+// name = "John";
+// admin = name;
+// alert(admin);
+
+// javascript.info Task 2
+// let ourPlanetName = "Earth";
+// let currentUserName;
+
+// javascript.info Task 3
+// const BIRTHDAY = '18.04.1982';
+// const age = someCode(BIRTHDAY);
