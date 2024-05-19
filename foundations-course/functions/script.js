5fb6c65f3be87c77f1b0ce43e4b7267614e1d243
Discusser <47938380+Discusser@users.noreply.github.com>
2024-05-19 19:24:07 +0200

    Complete functions assignment

---
+++ foundations-course/functions/script.js
@@ -0,0 +1,23 @@
+function add7(num) {
+    return num + 7;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function capitalize(string) {
+    return string.at(0).toUpperCase() + string.slice(1);
+}
+
+function lastLetter(string) {
+    return string.at(-1);
+}
+
+const add = function(a, b) {
+    return a + b;
+};
+
+const subtract = (a, b) => {
+    return a - b;
+}
