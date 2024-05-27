ea1ba30befc118c10d78b7f5ca3689d80028b7ed
Discusser <47938380+Discusser@users.noreply.github.com>
2024-05-27 22:13:44 +0200

    (calculator) Write HTML for calculator

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,3 +1,8 @@
-function add(a, b) {
-    return a + b;
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(a, b, operator) {
+    return operator(a, b);
 }
