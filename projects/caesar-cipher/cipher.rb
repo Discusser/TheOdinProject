5e96c66d4500a439a51fa277c4d5d64509815c90
Discusser <47938380+Discusser@users.noreply.github.com>
2024-06-16 22:20:46 +0200

    Add caesar-cipher project

---
+++ projects/caesar-cipher/cipher.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(string, shift_factor)
+  string.split("").map do |character|
+    shifted_ordinal = character.ord
+
+    lowercase = ("a".."z").include?(character)
+    uppercase = ("A".."Z").include?(character)
+    if lowercase || uppercase
+      shifted_ordinal = (character.ord + shift_factor)
+      shifted_ordinal -= 26 while shifted_ordinal > (lowercase ? "z" : "Z").ord
+    end
+
+    shifted_ordinal.chr
+  end.join("")
+end
+
+p caesar_cipher("What a string!", 5)
