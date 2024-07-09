830c6655e9722d3f4f77bb652980b81fbc5e023a
Discusser <47938380+Discusser@users.noreply.github.com>
2024-07-09 12:41:32 +0200

    (event-manager) Complete assignements

--- projects/event-manager/lib/event_manager.rb
+++ projects/event-manager/lib/event_manager.rb
@@ -6,6 +6,16 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def clean_phone_number(phone_number)
+  stripped = phone_number.gsub(/[().\- ]/, "")
+  digits = stripped.scan(/\d/).size
+  if digits > 11 && phone_number.starts_with?("1")
+    stripped[1..]
+  elsif digits == 10
+    stripped
+  end
+end
+
 def legislators_by_zipcode(zipcode) # rubocop:disable Metrics/MethodLength
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = File.read("secret.key").strip
@@ -19,6 +29,11 @@ def legislators_by_zipcode(zipcode) # rubocop:disable Metrics/MethodLength
   rescue StandardError
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
+end
+
+def parse_date(date)
+  # 11/25/08 19:21
+  DateTime.strptime(date, "%D %R")
 end
 
 def save_thank_you_letter(id, form_letter)
@@ -39,6 +54,8 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:homephone])
+  registration_date = parse_date(row[:regdate])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
