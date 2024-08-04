51ecd87ba61937fda9bdc067801ddc909dd143fe
Discusser <47938380+Discusser@users.noreply.github.com>
2024-08-04 10:08:09 +0200

    Refactor caesar-cipher into a testable class

--- projects/caesar-cipher/lib/cipher.rb
+++ projects/caesar-cipher/lib/cipher.rb
@@ -1,27 +1,36 @@
-def shift_value(value, shift_factor, min, max)
-  range = max - min + 1
-  shifted_value = value + shift_factor
-  shifted_value -= range while shifted_value > max
-  shifted_value += range while shifted_value < min
-  shifted_value
-end
+class CaesarCipher
+  attr_accessor :message, :shift_factor
 
-def shift_character(character, shift_factor)
-  shifted_ordinal = character.ord
-
-  lowercase = ("a".."z").include?(character)
-  uppercase = ("A".."Z").include?(character)
-  if lowercase
-    shifted_ordinal = shift_value(shifted_ordinal, shift_factor, "a".ord, "z".ord)
-  elsif uppercase
-    shifted_ordinal = shift_value(shifted_ordinal, shift_factor, "A".ord, "Z".ord)
+  def initialize(message, shift_factor)
+    @message = message
+    @shift_factor = shift_factor
   end
 
-  shifted_ordinal.chr
-end
+  def shift_string
+    @message.chars.map { |character| shift_character(character, @shift_factor) }.join
+  end
 
-def caesar_cipher(string, shift_factor)
-  string.chars.map { |character| shift_character(character, shift_factor) }.join
-end
+  private
 
-p caesar_cipher("abcd", -34)
+  def shift_value(value, shift_factor, min, max)
+    range = max - min + 1
+    shifted_value = value + shift_factor
+    shifted_value -= range while shifted_value > max
+    shifted_value += range while shifted_value < min
+    shifted_value
+  end
+
+  def shift_character(character, shift_factor)
+    shifted_ordinal = character.ord
+
+    lowercase = ("a".."z").include?(character)
+    uppercase = ("A".."Z").include?(character)
+    if lowercase
+      shifted_ordinal = shift_value(shifted_ordinal, shift_factor, "a".ord, "z".ord)
+    elsif uppercase
+      shifted_ordinal = shift_value(shifted_ordinal, shift_factor, "A".ord, "Z".ord)
+    end
+
+    shifted_ordinal.chr
+  end
+end
