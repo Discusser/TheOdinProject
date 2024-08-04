4931349059ea6571779b8122496a250dbc6dff05
Discusser <47938380+Discusser@users.noreply.github.com>
2024-08-04 10:03:41 +0200

    Refactor caesar-cipher code

--- projects/caesar-cipher/lib/cipher.rb
+++ projects/caesar-cipher/lib/cipher.rb
@@ -1,16 +1,27 @@
-def caesar_cipher(string, shift_factor)
-  string.chars.map do |character|
-    shifted_ordinal = character.ord
-
-    lowercase = ("a".."z").include?(character)
-    uppercase = ("A".."Z").include?(character)
-    if lowercase || uppercase
-      shifted_ordinal = (character.ord + shift_factor)
-      shifted_ordinal -= 26 while shifted_ordinal > (lowercase ? "z" : "Z").ord
-    end
-
-    shifted_ordinal.chr
-  end.join
+def shift_value(value, shift_factor, min, max)
+  range = max - min + 1
+  shifted_value = value + shift_factor
+  shifted_value -= range while shifted_value > max
+  shifted_value += range while shifted_value < min
+  shifted_value
 end
 
-p caesar_cipher("What a string!", 5)
+def shift_character(character, shift_factor)
+  shifted_ordinal = character.ord
+
+  lowercase = ("a".."z").include?(character)
+  uppercase = ("A".."Z").include?(character)
+  if lowercase
+    shifted_ordinal = shift_value(shifted_ordinal, shift_factor, "a".ord, "z".ord)
+  elsif uppercase
+    shifted_ordinal = shift_value(shifted_ordinal, shift_factor, "A".ord, "Z".ord)
+  end
+
+  shifted_ordinal.chr
+end
+
+def caesar_cipher(string, shift_factor)
+  string.chars.map { |character| shift_character(character, shift_factor) }.join
+end
+
+p caesar_cipher("abcd", -34)
